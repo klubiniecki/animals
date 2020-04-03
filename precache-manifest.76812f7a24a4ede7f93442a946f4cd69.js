05aac231eb950e37735c1a851a69681f2da3af64
klubiniecki <klubiniecki@gmail.com>
2020-04-03 15:44:18 +1100

    Updates

---
+++ precache-manifest.76812f7a24a4ede7f93442a946f4cd69.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8411c283a68512f3e82f46d676745c",
+    "url": "/animals/index.html"
+  },
+  {
+    "revision": "cae78c60dcfbc440c79f",
+    "url": "/animals/static/css/main.a1815a4e.chunk.css"
+  },
+  {
+    "revision": "11fd40c348b671932788",
+    "url": "/animals/static/js/2.d50d7172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animals/static/js/2.d50d7172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986a91f901326040433f",
+    "url": "/animals/static/js/3.4c2be952.chunk.js"
+  },
+  {
+    "revision": "11632a58e510552bf3ae8fe3d05874e6",
+    "url": "/animals/static/js/3.4c2be952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae78c60dcfbc440c79f",
+    "url": "/animals/static/js/main.308f20b3.chunk.js"
+  },
+  {
+    "revision": "1d3e8a792e21620c1943",
+    "url": "/animals/static/js/runtime-main.ed3ba232.js"
+  },
+  {
+    "revision": "0f7bb0d1a5ffff21647a8d1116c49636",
+    "url": "/animals/static/media/bear.0f7bb0d1.jpeg"
+  },
+  {
+    "revision": "a7110b58c3db736aac215e1edf6bd115",
+    "url": "/animals/static/media/bear.a7110b58.mp3"
+  },
+  {
+    "revision": "2bc244df68314b88f89f2baabe2857aa",
+    "url": "/animals/static/media/cat.2bc244df.png"
+  },
+  {
+    "revision": "40609de54e0a86c89673e0723bc9c1ff",
+    "url": "/animals/static/media/cat.40609de5.mp3"
+  },
+  {
+    "revision": "2679e90dc3bda051023cba5d657e6c73",
+    "url": "/animals/static/media/chicken.2679e90d.mp3"
+  },
+  {
+    "revision": "187a0954fdff44de65c5fd22c5fbdc7a",
+    "url": "/animals/static/media/cock.187a0954.mp3"
+  },
+  {
+    "revision": "6e5bd1ec9857767783b80ae022a9a095",
+    "url": "/animals/static/media/cock.6e5bd1ec.jpg"
+  },
+  {
+    "revision": "5cdec1ab4ffb0ad6f3065e46280c73d5",
+    "url": "/animals/static/media/cow.5cdec1ab.jpeg"
+  },
+  {
+    "revision": "660c70fc35023db1f34698cc2473b4e1",
+    "url": "/animals/static/media/cow.660c70fc.mp3"
+  },
+  {
+    "revision": "d6f1a9ce572f5f3cc1bd9fd8e3bbf5fd",
+    "url": "/animals/static/media/dog.d6f1a9ce.mp3"
+  },
+  {
+    "revision": "587550264f46cfd093f909ef98e0a43e",
+    "url": "/animals/static/media/dolphin.58755026.mp3"
+  },
+  {
+    "revision": "845eaa05228bb24f11c949534d63907b",
+    "url": "/animals/static/media/donkey.845eaa05.jpeg"
+  },
+  {
+    "revision": "a8450d2c472897797b93342984d8be0b",
+    "url": "/animals/static/media/donkey.a8450d2c.mp3"
+  },
+  {
+    "revision": "97f701be97a6cee451bb9239ffcdbe35",
+    "url": "/animals/static/media/duck.97f701be.mp3"
+  },
+  {
+    "revision": "a6395aedf7c052edb7f835783d523db4",
+    "url": "/animals/static/media/eagle.a6395aed.mp3"
+  },
+  {
+    "revision": "b468528dac3be1193d111e2286f57e64",
+    "url": "/animals/static/media/elephant.b468528d.mp3"
+  },
+  {
+    "revision": "724b0642774b41b4edc4bca89e7bd8bb",
+    "url": "/animals/static/media/frog.724b0642.jpg"
+  },
+  {
+    "revision": "eb70ad45f3dc64a807224aea5eb8d105",
+    "url": "/animals/static/media/frog.eb70ad45.mp3"
+  },
+  {
+    "revision": "bad89bf3419f37f70b6e4945117ba5d1",
+    "url": "/animals/static/media/goat.bad89bf3.mp3"
+  },
+  {
+    "revision": "6a84a1bc825bbba57616f741641f5e54",
+    "url": "/animals/static/media/goose.6a84a1bc.mp3"
+  },
+  {
+    "revision": "d1da04347eae57a74d2887af6ec9e994",
+    "url": "/animals/static/media/goose.d1da0434.png"
+  },
+  {
+    "revision": "5bea9c7fc6d66b76839fa1562da7889a",
+    "url": "/animals/static/media/gorilla.5bea9c7f.mp3"
+  },
+  {
+    "revision": "a9fe1f417ad4fe74e945507cad966e53",
+    "url": "/animals/static/media/horse.a9fe1f41.mp3"
+  },
+  {
+    "revision": "d5d462951760350be0bfc47b6d3733e9",
+    "url": "/animals/static/media/horse.d5d46295.jpeg"
+  },
+  {
+    "revision": "2ef2241180591dba8099b1a5c7c195cb",
+    "url": "/animals/static/media/koala.2ef22411.jpg"
+  },
+  {
+    "revision": "b70ea97d64db3e00e12f20415e741c3b",
+    "url": "/animals/static/media/koala.b70ea97d.mp3"
+  },
+  {
+    "revision": "017e4b7c655fa4c5960be555618274d4",
+    "url": "/animals/static/media/kookabura.017e4b7c.mp3"
+  },
+  {
+    "revision": "431b8263ffd1563d6d781bc048a18900",
+    "url": "/animals/static/media/lion.431b8263.mp3"
+  },
+  {
+    "revision": "b557994c67380eb573c9bf29cad8db16",
+    "url": "/animals/static/media/magpie.b557994c.jpg"
+  },
+  {
+    "revision": "f12d0ad15fe24192a887d7f9f23bb92f",
+    "url": "/animals/static/media/magpie.f12d0ad1.mp3"
+  },
+  {
+    "revision": "a920bac46c55fb3f8480a81c9f3fcc85",
+    "url": "/animals/static/media/monkey.a920bac4.jpg"
+  },
+  {
+    "revision": "bb0f6969eebd66e9ab69aabbedd829e7",
+    "url": "/animals/static/media/monkey.bb0f6969.mp3"
+  },
+  {
+    "revision": "40a80087f38e397226cacb10bfb5d761",
+    "url": "/animals/static/media/mouse.40a80087.mp3"
+  },
+  {
+    "revision": "3fcfc152ce9795dea3c35999399243ee",
+    "url": "/animals/static/media/owl.3fcfc152.jpg"
+  },
+  {
+    "revision": "e92db1a3585ce02ac904e8d1097a2d3c",
+    "url": "/animals/static/media/owl.e92db1a3.mp3"
+  },
+  {
+    "revision": "b0781153e78a5fbb8403aaa31168974f",
+    "url": "/animals/static/media/pig.b0781153.mp3"
+  },
+  {
+    "revision": "4bf0cecf5583e09808c4ea8507f80329",
+    "url": "/animals/static/media/pigeon.4bf0cecf.mp3"
+  },
+  {
+    "revision": "1ac0c5b377eab429bde8e269c5a3ea00",
+    "url": "/animals/static/media/sheep.1ac0c5b3.png"
+  },
+  {
+    "revision": "ded305e071a51525bf20ecba2a7956ed",
+    "url": "/animals/static/media/sheep.ded305e0.mp3"
+  }
+]);
